c8be254c3399ed4f031e2d075339f00b1ca77c90
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 23:07:30 +0800

    adjust sql

--- form.sql
+++ form.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2020-11-21 09:48:52
+-- 產生時間： 2020-11-21 16:06:50
 -- 伺服器版本： 10.4.11-MariaDB
 -- PHP 版本： 7.4.6
 
@@ -33,24 +33,31 @@ CREATE TABLE `form` (
   `dad` text NOT NULL,
   `mom` text NOT NULL,
   `species` text NOT NULL,
-  `status` int(10) NOT NULL,
   `t_content` text NOT NULL,
   `t_sign` text NOT NULL,
-  `s_result` int(100) NOT NULL,
+  `s_result` int(100) DEFAULT NULL,
+  `s_content` text NOT NULL,
   `s_sign` text NOT NULL,
-  `p_sign` text NOT NULL
+  `p_sign` text NOT NULL,
+  `status` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- 傾印資料表的資料 `form`
 --
 
-INSERT INTO `form` (`id`, `name`, `dad`, `mom`, `species`, `status`, `t_content`, `t_sign`, `s_result`, `s_sign`, `p_sign`) VALUES
-(2, 'c', 'd', 'd', '中低收入戶', 0, '', '', 0, '', ''),
-(3, 'c', 'd', 'd', '家庭突發因素', 0, '', '', 0, '', ''),
-(4, 'c', 'd', 'd', '家庭突發因素', 0, '', '', 0, '', ''),
-(9, 'q', 's', 's', '中低收入戶', 0, '', '', 0, '', ''),
-(8, 'q', 's', 's', '中低收入戶', 0, '', '', 0, '', '');
+INSERT INTO `form` (`id`, `name`, `dad`, `mom`, `species`, `t_content`, `t_sign`, `s_result`, `s_content`, `s_sign`, `p_sign`, `status`) VALUES
+(1, 'a', 'a', 'a', '低收入戶', 'a', 'a', 123, 'a', 'a', 'a', '決行(結案)');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `form`
+--
+ALTER TABLE `form`
+  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
