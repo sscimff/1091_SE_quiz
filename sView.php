70959e8c229c2035109d3e8085c1af7d14a185ab
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 21:07:10 +0800

    commit

--- sView.php
+++ sView.php
@@ -1,37 +1,3 @@
-<<<<<<< HEAD:sView.php
-<?php
-session_start();
-require("todoStudent.php");
-
-$id = (int)$_GET['id'];
-$rs = getJobDetail($id);
-if (! $rs) {
-    echo "no data found";
-    exit(0);
-}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Secretary</title>
-</head>
-<body>
-<h1>Secretary Task</h1>
-<form method="post" action="todoStudent.php">
-
-      秘書審核結果:准予補助 <input name="msg" type="text" id="msg" /> 元(若輸入0元則不予以補助)<br>
-      秘書審查意見: <input name="msg" type="text" id="msg" /> <br>
-      秘書簽章: <input name="msg" type="text" id="msg" /> <br>
-      
-      <input type="submit" name="Submit" value="送出" />
-      <br>
-</form>
-</tr>
-</table>
-</body>
-</html>
-=======
 <?php
 session_start();
 require("todoStudent.php");
@@ -64,4 +30,3 @@ if (! $rs) {
 </table>
 </body>
 </html>
->>>>>>> 8707be2d78d61afcbe9cc28cf5c51e0399ef4838:sViex.php
