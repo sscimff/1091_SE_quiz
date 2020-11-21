ea7cd4ffb761d2f0e6d3195579f04bf461ee5ce0
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 22:11:39 +0800

    status

--- tModel.php
+++ tModel.php
@@ -11,7 +11,7 @@ function add($t_content, $t_sign, $id) {
 
 function show($id) {
     global $conn;
-    $sql = "UPDATE form SET t_content=$t_content, t_sign=$t_sign, status='秘書尚未簽注' WHERE id=$id and (status='導師尚未簽注' or status='秘書尚未簽注');";
+    $sql = "SELECT * FROM form WHERE id=$id and (status='導師尚未簽注' or status='秘書尚未簽注');";
     mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 }
 
