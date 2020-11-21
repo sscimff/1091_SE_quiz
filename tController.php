af027040deca6d5f89cfcea3e5301bd8b168c67c
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-22 00:07:49 +0800

    adjust

--- tController.php
+++ tController.php
@@ -5,7 +5,6 @@ $id=$_POST['id'];
 $t_content=mysqli_escape_string($conn, $_POST['t_content']);
 $t_sign=mysqli_escape_string($conn, $_POST['t_sign']);
 t_add($t_content, $t_sign, $id);
-// echo "Message:".$_POST['id'] ."completed.";
 echo "Message: Completed.";
 ?>
 
