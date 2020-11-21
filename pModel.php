71bdfcb2e80ba2d64531032cb0863257b2cb3a4a
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 22:58:46 +0800

    補逗號

--- pModel.php
+++ pModel.php
@@ -5,7 +5,7 @@ require_once("dbconnect.php");
 // 校長核決
 function close($status, $p_sign, $id) {
     global $conn;
-    $sql = "UPDATE form SET p_sign='$p_sign' status='$status' WHERE id=$id;";
+    $sql = "UPDATE form SET p_sign='$p_sign', status='$status' WHERE id=$id;";
     mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 }
 
