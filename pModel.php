c9614864514e1f2a36530d1da3f401ed415dfb6e
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 21:50:25 +0800

    adjust

--- pModel.php
+++ pModel.php
@@ -2,10 +2,16 @@
 // Model 就是 SQL 指令(跟資料庫有關的變動)
 require_once("dbconnect.php");
 
-// 校長簽核
+// 校長核決(結案)
 function close($p_sign, $id) {
     global $conn;
-    $sql = "UPDATE form SET p_sign=$p_sign status='結案' WHERE id=$id and status=2);";
+    $sql = "UPDATE form SET p_sign=$p_sign status='結案' WHERE id=$id and status='秘書尚未簽注');";
+    mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
+}
+// 校長否決
+function deny($p_sign, $id) {
+    global $conn;
+    $sql = "UPDATE form SET p_sign=$p_sign status='否決' WHERE id=$id and status=2);";
     mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 }
 ?>
--- pView.php
+++ pView.php
@@ -1,13 +1,6 @@
 <?php
 session_start();
-require("todoStudent.php");
-
-$id = (int)$_GET['id'];
-$rs = getJobDetail($id);
-if (! $rs) {
-	echo "no data found";
-	exit(0);
-}
+require("dbconnect.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -18,12 +11,11 @@ if (! $rs) {
 <body>
 <h1>Principal Task</h1>
 <form method="post" action="studentController.php">
-      校長簽核:<select>
+      校長簽核：<select>
                 <option>決行</option>
                 <option>否決</option>
                 </select>
       <input name="msg" type="text" id="msg" /> <br>
-	  
       <input type="submit" name="Submit" value="送出" />
       <br>
 </form>
