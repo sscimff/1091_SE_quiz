bee911523c93a4802ed7987654da000a9a293aaf
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 19:58:10 +0800

    新增t_model

---
+++ tModel.php
@@ -0,0 +1,11 @@
+<?php
+// Model 就是 SQL 指令(跟資料庫有關的變動)
+require_once("dbconnect.php");
+
+// 導師簽注
+function add($t_content, $t_sign, $id) {
+    global $conn;
+    $sql = "UPDATE form SET t_content=$t_content, t_sign=$t_sign WHERE id=$id and status=0;";
+    mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
+}
+?>
