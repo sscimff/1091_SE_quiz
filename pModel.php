5fc42d5ac77ab33f3e773ab855bab88a0abbc33c
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 20:26:57 +0800

    status

--- pModel.php
+++ pModel.php
@@ -2,10 +2,10 @@
 // Model 就是 SQL 指令(跟資料庫有關的變動)
 require_once("dbconnect.php");
 
-// 導師簽注
-function add($t_content, $t_sign, $id) {
+// 校長簽核
+function add($p_sign, $id) {
     global $conn;
-    $sql = "UPDATE form SET t_content=$t_content, t_sign=$t_sign, status=1 WHERE id=$id and (status=0 or status=1);";
+    $sql = "UPDATE form SET p_sign=$p_sign status=3 WHERE id=$id and status=2);";
     mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 }
 ?>
--- sModel.php
+++ sModel.php
@@ -5,7 +5,7 @@ require_once("dbconnect.php");
 // 秘書簽注
 function add($s_result, $s_sign, $id) {
     global $conn;
-    $sql = "UPDATE form SET s_result=$s_result, s_sign=$s_sign WHERE id=$id and status=2;";
+    $sql = "UPDATE form SET s_result=$s_result, s_sign=$s_sign status=2 WHERE id=$id and (status=1 or status=2);";
     mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 }
 ?>
