9cbb3e0066df1a80ac041f2267259935ad587962
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 22:14:13 +0800

    adjust

--- tModel.php
+++ tModel.php
@@ -3,13 +3,13 @@
 require_once("dbconnect.php");
 
 // 導師簽注
-function add($t_content, $t_sign, $id) {
+function t_add($t_content, $t_sign, $id) {
     global $conn;
     $sql = "UPDATE form SET t_content=$t_content, t_sign=$t_sign, status='秘書尚未簽注' WHERE id=$id and (status='導師尚未簽注' or status='秘書尚未簽注');";
     mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 }
 
-function show($id) {
+function t_show($id) {
     global $conn;
     $sql = "SELECT * FROM form WHERE id=$id and (status='導師尚未簽注' or status='秘書尚未簽注');";
     mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
