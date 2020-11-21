737a0488189434e875412976603c62bb2f75e954
s107213013 <s107213013@mail1.ncnu.edu.tw>
2020-11-21 20:00:44 +0800

    commit

--- tView.php
+++ tView.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require("dbconnect.php");
+require("tModel.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -13,9 +13,8 @@ require("dbconnect.php");
 <form method="post" action="studentController.php">
 
       導師訪視說明: <input name="t_content" type="text" id="t_content" /> <br>
-
       導師簽章: <input name="t_sign" type="text" id="t_sign" /> <br>
-	  
+
       <input type="submit" name="Submit" value="送出" />
       <br>
 </form>
